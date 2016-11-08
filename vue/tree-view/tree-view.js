b391fd353c9ef5f5da82b2bc0efde97a39895253
fangzi <szfangziwei@qq.com>
2016-11-08 21:45:44 +0800

    s

--- vue/tree-view/tree-view.js
+++ vue/tree-view/tree-view.js
@@ -11,13 +11,24 @@ $.ajax({
 Vue.component('treeview', {
     name:'treeview',
     template:'<div v-for="item in data" class="node">' +
-        '<template v-if="item.childs">' +
+    '<template v-if="item.childs">' +
+    '<span v-on:click="nodeToggle($index)">' +
     '<b v-show="item.expand">-</b><b v-else>+</b>' +
+    '{{item.name}}</span>' +
     '</template>'+
-    '<span>{{item.name}}</span>' +
-    '<treeview v-if="item.childs" v-bind:data="item.childs" ></treeview>' +
+    '<template v-else>'+
+    '<span>*{{item.name}}</span>' +
+    '</template>'+
+    '<div class="node-list" v-bind:class="{show:item.expand}">'+
+    '<treeview v-if="item.childs" v-bind:data="item.childs"></treeview>' +
+    '</div>'+
     '</div>',
-    props:['data']
+    props:['data'],
+    methods:{
+        nodeToggle: function (index) {
+            this.data[index].expand = !this.data[index].expand;
+        }
+    }
 
 });
 
@@ -25,13 +36,5 @@ var vm = new Vue({
     el:'#tree-view',
     data:{
         treeData:treeData
-    },
-    methods:{
-        open: function (index) {
-
-        },
-        close: function (index) {
-
-        }
     }
 });
